b252bde7c730a4544100f3d5b69b52cb2d2ae424
Thulio Pereira <tulio_1996@yahoo.com>
2023-07-17 01:05:05 -0300

    17/07

---
+++ Exercicios/ex003.py
@@ -0,0 +1,17 @@
+print("SEM ESPECIFICAR O TIPO PRIMITIVO DO INPUT")
+n1 = (input("Digite um valor: "))
+n2 = (input("Digite outro valor: "))
+s1 = n1 + n2
+print(s1)
+print(type(s1))
+
+print("-------------------------------------------------")
+
+print("ESPECIFICANDO O TIPO PRIMITIVO DO INPUT")
+n3 = int(input("Digite um valor: "))
+n4 = int(input("Digite outro valor: "))
+
+s2 = n3 + n4
+
+print(s2)
+print(type(s2))
---
+++ Exercicios/ex004.py
@@ -0,0 +1,13 @@
+letraMinuscula = "ola"
+letraMaiuscula = "OLA"
+letraMista = "Ola"
+numeroInt = "1"
+numeroComLetra = "Durango 96"
+
+var = letraMista
+
+print("É ALFANUMERICO?: ", var.isalpha())
+print("É NUMERICO?: ", var.isnumeric())
+print("É DECIMAL?: ", var.isdecimal())
+print("É MINUSCULO?: ", var.islower())
+print("É MAIUSCULO?: ", var.isupper())
